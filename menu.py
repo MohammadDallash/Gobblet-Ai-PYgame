17947019350916fb172a4698b1ed136a75b171a5
RanaHossny <104026566+RanaHossny@users.noreply.github.com>
2023-11-29 18:06:39 +0200

    update

--- menu.py
+++ menu.py
@@ -85,10 +85,9 @@ class MainMenu(Menu):
 
 
 
-class OptionsMenu(Menu):
+class OptionsMenu(Menu): #Todo: make options menu : game_mode : computer-to-computer or player-to-player or player-to-computer , difficulty : easy or hard , volume : 0-100
     def __init__(self, game):
         Menu.__init__(self, game)
-        self.state = 'Volume'
         self.volx, self.voly = self.mid_w, self.mid_h + 20
         self.controlsx, self.controlsy = self.mid_w, self.mid_h + 40
         self.cursor_rect.midtop = (self.volx + self.offset, self.voly)
@@ -100,8 +99,6 @@ class OptionsMenu(Menu):
             self.check_input()
             self.game.display.fill(self.game.BLACK)
             self.game.draw_text('Options', 20, self.game.DISPLAY_W / 2, self.game.DISPLAY_H / 2 - 30)
-            self.game.draw_text("Volume", 20, self.volx, self.voly)
-            self.game.draw_text("Controls", 20, self.controlsx, self.controlsy)
             self.draw_cursor()
             self.blit_screen()
 
@@ -109,15 +106,6 @@ class OptionsMenu(Menu):
         if self.game.BACK_KEY:
             self.game.curr_menu=self.game.main_menu
             self.run_display=False
-        elif self.game.UP_KEY or self.game.DOWN_KEY:
-            if self.state == 'Volume':
-                self.state = 'Controls'
-                self.cursor_rect.midtop = (self.controlsx + self.offset, self.controlsy)
-            elif self.state == 'Controls':
-                self.state = 'Volume'
-                self.cursor_rect.midtop = (self.volx + self.offset, self.voly)
-        elif self.game.START_KEY:
-            # TO-DO: Create a Volume Menu and a Controls Menu
             pass
 
 
--- util/music.py
+++ util/music.py
@@ -5,24 +5,19 @@ class MusicPlayer:
         self.current_track = None
 
     def load_track(self, track_path):
-        # Load a music track
         pygame.mixer.music.load(track_path)
         self.current_track = track_path
 
     def play(self, loop=-1):
-        # Start playing the loaded track
         pygame.mixer.music.play(loop)
 
     def pause(self):
-        # Pause the currently playing track
         pygame.mixer.music.pause()
 
     def unpause(self):
-        # Unpause the currently paused track
         pygame.mixer.music.unpause()
 
     def stop(self):
-        # Stop the currently playing track
         pygame.mixer.music.stop()
 
     def set_volume(self, volume):
