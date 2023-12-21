946b99b596aa7d866891fa7ef135d6208f4e9e5e
hossamadel7 <57440281+hossamadel7@users.noreply.github.com>
2023-12-21 19:02:28 +0200

    Making the board shows

--- states/playing.py
+++ states/playing.py
@@ -10,8 +10,10 @@ class Playing(State):
         self.spritesheet = Spritesheet('assets\sprites\sprites.png')
         self.map = TileMap('assets\sprites\map.csv', self.spritesheet)
         self.turn = 1
-        self.players_names = ['player1', 'player2']
+        self.players_names = ['player 1', 'player 2']
         self.turn_text =  self.players_names[self.turn] + ' Turn'
+                                                                       ## 1 3 5
+        self.board = [[2,1,4,-1],[-1,3,4,-1], [0,1,4,-1],[1,-1,5,-1]] ## 0 2 4 for sizes and odd even for black, white
 
 
 
@@ -21,6 +23,8 @@ class Playing(State):
 
         
     def update(self, delta_time, actions):
+        self.map.reconstruct_map(self.board)
+
         if(actions['Esc']):
            self.exit_state()
     
@@ -29,7 +33,7 @@ class Playing(State):
     def render(self,display):
         display.fill(self.game.BROWN)
         self.helper.draw_text(display,self.turn_text,self.game.WHITE ,20, self.game.DISPLAY_W / 2, 30)
-        self.map.draw_map(display)
+        self.map.draw_map_on_canvis(display)
 
 
 
