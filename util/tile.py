60c34cbbd19153b5d0097b1ce38f93e3ed03e33f
Kerolos Sameh El-Shaip Boles <104724781+KahrabaVv@users.noreply.github.com>
2023-12-21 15:40:06 +0200

    Add files via upload

---
+++ util/tile.py
@@ -0,0 +1,65 @@
+import pygame, csv, os
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, imageIdx, x, y, spritesheet):
+        pygame.sprite.Sprite.__init__(self)
+        
+        self.imgIds = spritesheet.keys_list
+
+        self.image = spritesheet.parse_sprite(self.imgIds[imageIdx])
+        # Manual load in: self.image = pygame.image.load(image)
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = x, y
+
+    def draw(self, surface):
+        surface.blit(self.image, (self.rect.x, self.rect.y))
+
+class TileMap():
+    def __init__(self, filename, spritesheet):
+        self.tile_size = 120
+        self.spritesheet = spritesheet
+        self.tiles = self.load_tiles(filename)
+        self.map_surface = pygame.Surface((self.map_w, self.map_h))
+        self.map_surface.set_colorkey((0, 0, 0))
+        self.load_map()
+
+    def draw_map(self, surface):
+        surface.blit(self.map_surface, (0, 0))
+
+    def load_map(self):
+        for tile in self.tiles:
+            tile.draw(self.map_surface)
+
+    def read_csv(self, filename):
+        map = []
+        with open(os.path.join(filename)) as data:
+            data = csv.reader(data, delimiter=',')
+            for row in data:
+                map.append(list(row))
+        return map
+
+    def load_tiles(self, filename):
+        tiles = []
+        map = self.read_csv(filename)
+        x, y = 0, 0
+        for row in map:
+            x = 0
+            for tile in row:
+                if(tile != '-1'):
+                    tiles.append(Tile(int(tile), x * self.tile_size, y * self.tile_size, self.spritesheet))
+                x += 1
+
+            # Move to next row
+            y += 1
+            # Store the size of the tile map
+        self.map_w, self.map_h = x * self.tile_size, y * self.tile_size
+        return tiles
+
+
+
+
+
+
+
+
+
