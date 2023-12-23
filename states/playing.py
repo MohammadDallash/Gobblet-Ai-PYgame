c52ac1c125e0c83b45497ed3027507b788069492
Sara Hossny <1901083@eng.asu.edu.eg>
2023-12-24 01:52:00 +0200

    mouse handle and gui update

--- states/playing.py
+++ states/playing.py
@@ -27,9 +27,16 @@ class Playing(State):
         self.players_names = ['Player 1', 'Player 2']
         self.turn_text =  self.players_names[self.turn] + ' Turn'
         self.game_started = False 
+        self.mouse_is_pressed = False
+        self.selected_tile = None  # Stores (inventory_row, tile_value)
+        self.selected_tile_original_pos = None  # Stores original position in inventory
+        self.result = None
+        self.mouse_pos = (0, 0)
+        self.selected_shape=False
+        self.highlighted_tile_rect = None
         # Initial board (for testing)                                                             
         self.board = [
-                    [BLACK_SMALL,BLACK_MEDIUM, BLACK_LARGE,BLACK_XLARGE],
+                    [EMPTY_TILE,BLACK_MEDIUM, BLACK_LARGE,BLACK_XLARGE],
                     [WHITE_SMALL,WHITE_MEDIUM, WHITE_LARGE,WHITE_XLARGE],
                     [BLACK_SMALL,WHITE_SMALL, BLACK_LARGE,EMPTY_TILE],
                     [WHITE_SMALL,EMPTY_TILE,  WHITE_LARGE,EMPTY_TILE]] 
@@ -40,22 +47,123 @@ class Playing(State):
         self.map.reconstruct_map(self.board)
     def update(self, delta_time, actions):
         #TODO() add check_wins() here 
-
         # draw an image only if a new event happens (like mouse movement) or if the game is just launched.
+        self.mouse_pos = pygame.mouse.get_pos()
         if len(pygame.event.get()) > 0 or not self.game_started :
             self.map.reconstruct_map(self.board)
-            self.map.reconstruct_inventory(self.inventory)
+            self.result=self.map.reconstruct_inventory(self.inventory)
             self.game_started = True
-        
+        if actions['LEFT_MOUSE_KEY_PRESS']:
+                self.handle_mouse_click(pygame.mouse.get_pos())
+                self.map.reconstruct_map(self.board)
+                self.result=self.map.reconstruct_inventory(self.inventory)    
+        if not actions['LEFT_MOUSE_KEY_PRESS'] and self.selected_tile:
+            self.handle_mouse_release(pygame.mouse.get_pos())
+            self.map.reconstruct_map(self.board)
+            self.result=self.map.reconstruct_inventory(self.inventory)    
+            
         if(actions['Esc']):
            self.exit_state()
-    
-    def render(self,display):
-        display.fill(self.game.BROWN)
-        # display a the current turn text on the top of the screen.
-        self.helper.draw_text(display,self.turn_text,self.game.WHITE ,20, self.game.DISPLAY_W / 2, 30)
 
+    def handle_mouse_click(self, pos):
+        if self.is_click_on_inventory(pos) and not self.selected_shape:
+            self.selected_shape = True
+            self.handle_inventory_click(pos)
+        elif self.selected_shape :
+            self.near_from_board(pos)
+            
+            
+        
+    def is_click_on_inventory(self, pos):
+        for tile_list in self.result:  
+            for tile in tile_list:
+                if tile.rect.collidepoint(pos):
+                    return True
+        return False
+    def near_from_board(self, pos):
+        for row in self.map.tiles_board_positions:
+            for tile_info in row:
+                tile_rect = pygame.Rect(tile_info['x'], tile_info['y'], tile_info['width'], tile_info['height'])
+                if tile_rect.collidepoint(pos):
+                    # Store the rectangle information instead of drawing
+                    self.highlighted_tile_rect = tile_rect
+                    return
+        self.highlighted_tile_rect = None  # Reset if no tile is highlighted   
+
+    def handle_inventory_click(self, pos):
+        # Step 1: Determine which inventory tile and player's inventory is clicked
+        clicked_inventory = None
+        player = None
+        for i, tile_list in enumerate(self.result):  # Assuming result contains inventory tiles
+            for j, tile in enumerate(tile_list):
+                if tile.rect.collidepoint(pos):
+                    clicked_inventory = (j, i)
+                    player = i  # 0 for black, 1 for white
+                    inventory_num=j
+                    break
+            if clicked_inventory is not None:
+                break
+
+        # Check if a valid inventory tile was clicked
+        if clicked_inventory is None:
+            return
+
+        # Step 2 and 3: Find and pick the largest piece
+        player_inventory = self.inventory[player]
+        largest_piece, updated_inventory = self.get_largest_piece(player_inventory[inventory_num])
+        # Step 4: Update the inventory and set the selected piece
+        self.inventory[player][inventory_num] = updated_inventory
+    
+        self.selected_tile = (player, largest_piece)
+        self.selected_tile_original_pos = clicked_inventory
+
+    def get_largest_piece(self, inventory_value):
+        # Masks for black and white pieces
+        masks = [8, 4, 2, 1, 128, 64, 32, 16]
+
+        # Find the largest piece
+        for mask in masks:
+            if inventory_value & mask:
+                # Remove the largest piece from the inventory
+                return mask, inventory_value - mask
+
+        return None, inventory_value
+    def handle_mouse_release(self,pos):
+        for row_idx, row in enumerate(self.map.tiles_board_positions):
+            for col_idx, tile_info in enumerate(row):
+                tile_rect = pygame.Rect(tile_info['x'], tile_info['y'], tile_info['width'], tile_info['height'])
+                if tile_rect.collidepoint(pos):
+                    
+                    # Update the board with the selected tile value
+                    if self.selected_tile:  # Ensure a tile is selected
+                        self.board[row_idx][col_idx] = self.selected_tile[1]
+                        self.selected_shape = False
+                        self.selected_tile = None
+                        self.highlighted_tile_rect = None
+                        return
+                        
+
+
+    def render(self, display):
+        # Step 1: Clear the screen
+        display.fill(self.game.BROWN)
+
+        # Step 2: Draw game elements
+        # Display the current turn text at the top of the screen
+        self.helper.draw_text(display, self.turn_text, self.game.WHITE, 20, self.game.DISPLAY_W / 2, 30)
         self.map.draw_map_on_canvas(display)
+        if self.highlighted_tile_rect:
+            pygame.draw.rect(display, (128, 0, 128), self.highlighted_tile_rect)
+        
+        if self.selected_tile:
+            self.map.selected_tile(self.selected_tile, self.mouse_pos).draw(display)
+        
+
+
+        # Step 4: Update the display
+        pygame.display.flip()  # or pygame.display.update()
+
+
 
     def enter_state(self):
         super().enter_state()
--- util/tile.py
+++ util/tile.py
@@ -1,7 +1,7 @@
 import pygame, csv, os
 from math import log2
 from util.helpers import *
-
+from util.tile import *
 # pieces for each color.
 EMPTY_TILE = -1
 BLACK_SMALL = 1
@@ -22,6 +22,8 @@ class Tile(pygame.sprite.Sprite):
         self.image = spritesheet.parse_sprite(self.imgIds[imageIdx]) # load an image from the spritesheet.
         self.rect = self.image.get_rect() # get the rect object and change its rectangular coordinates.
         self.rect.x, self.rect.y = x, y
+        self.rows = 0
+        self.cols = 0
     # draw a surface onto the screen.
     def draw(self, surface):
         surface.blit(self.image, (self.rect.x, self.rect.y))
@@ -39,6 +41,7 @@ class TileMap():
         self.map_surface.set_colorkey((0, 0, 0))
         self.board_surface.set_colorkey((0, 0, 0))
         self.inventory_surface.set_colorkey((0, 0, 0))
+        self.tiles_board_positions = []
         self.piece_to_idx = {
             EMPTY_TILE :   get_drawing_idx_on_Tilemap(EMPTY_TILE),
             BLACK_SMALL :  get_drawing_idx_on_Tilemap(BLACK_SMALL),
@@ -87,20 +90,40 @@ class TileMap():
             inv_white[i].draw(self.board_surface)
 
         # return the resulting tile list.
-        result = [inv_black,inv_white]
-        return result
-    
+        return  [inv_black,inv_white]
+    def selected_tile(self, selected_tile_value, position):
+     # Get the sprite for the selected tile
+     tile = Tile(self.piece_to_idx[selected_tile_value[1]], 0, 0, self.spritesheet) 
+     # Calculate the offset so the mouse is in the center of the tile
+     offset_x = tile.rect.width // 2
+     offset_y = tile.rect.height // 2   
+     # Update the tile's position
+     tile.rect.x = position[0] - offset_x
+     tile.rect.y = position[1] - offset_y   
+     return tile
 
-    # draw map on the screen.
     def reconstruct_map(self, board=None):
-        if(board!=None):
-            row,col = len(board), len(board[0])
-            for y in range (row):
-                for x in range (col):
-                    idx = self.piece_to_idx[board[y][x]]   
-                    T = (Tile( idx, (x+3) * self.tile_size, (y+1) * self.tile_size, self.spritesheet))
-                    T.draw(self.board_surface)
-        
+        self.tiles_board_position=[]
+        if board is not None:
+            row, col = len(board), len(board[0])
+            for y in range(row):
+                row_tiles = []
+                for x in range(col):
+                    idx = self.piece_to_idx[board[y][x]]
+                    tile_x = (x + 3) * self.tile_size
+                    tile_y = (y + 1) * self.tile_size    
+                    T = Tile(idx, tile_x, tile_y, self.spritesheet)
+                    T.draw(self.board_surface)   
+                    # Store the tile's position and size
+                    tile_info = {
+                        'x': tile_x,
+                        'y': tile_y,
+                        'width': self.tile_size,
+                        'height': self.tile_size
+                    }
+                    row_tiles.append(tile_info)  
+                self.tiles_board_positions.append(row_tiles)
+
 
     def read_csv(self, filename):
         map = []
@@ -109,21 +132,20 @@ class TileMap():
             for row in data:
                 map.append(list(row))
         return map
-
+    
     def load_initial_tiles(self, filename):
-        tiles = []
-        map = self.read_csv(filename)
-        x, y = 0, 0
-        for row in map:
-            x = 0
-            for tile in row:
-                tiles.append(Tile(int(tile), x * self.tile_size, y * self.tile_size, self.spritesheet))
-                x += 1
+            tiles = []
+            map_data = self.read_csv(filename)
+            x, y = 0, 0
+            for row in map_data:
+                x = 0
+                for tile in row:
+                    tiles.append(Tile(int(tile), x * self.tile_size, y * self.tile_size, self.spritesheet))
+                    x += 1
 
-            # Move to next row
-            y += 1
-            # Store the size of the tile map
-
-        self.map_w, self.map_h = x * self.tile_size, y * self.tile_size
-        return tiles
+                y += 1
+            # Store the size of the tile map and the number of rows and columns
+            self.map_w, self.map_h = x * self.tile_size, y * self.tile_size
+            self.cols, self.rows = x, y
+            return tiles
 
