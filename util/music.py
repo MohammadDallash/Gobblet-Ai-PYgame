b7bd65aa326cb8daa7ee68dbc524c8d2e45de85a
Sara hossny hassan <104026246+Sara-Hossny@users.noreply.github.com>
2024-01-16 22:19:38 +0200

    Fixed music and volume options, now works as intended

--- util/music.py
+++ util/music.py
@@ -1,34 +1,39 @@
 import pygame
 
-
 class MusicPlayer:
     def __init__(self):
         pygame.mixer.init()
         self.current_track = None
+        self.background_sound = None
+        self.sfx = pygame.mixer.Sound('assets/sound/move_piece.mp3')
+        self.volume = 1.0
 
     def load_track(self, track_path):
-
         pygame.mixer.music.load(track_path)
         self.current_track = track_path
+        self.background_sound = pygame.mixer.Sound(self.current_track)
+
 
     def play(self, loop=-1):
-        pygame.mixer.Channel(0).play(pygame.mixer.Sound(self.current_track),loops=-1)
+        pygame.mixer.Channel(0).play(self.background_sound,loops=-1)
 
     def pause(self):
-        pygame.mixer.music.pause()
+        pygame.mixer.pause()
 
     def unpause(self):
-        pygame.mixer.music.unpause()
+        pygame.mixer.unpause()
 
     def stop(self):
-        pygame.mixer.Channel(0).stop()
+        pygame.mixer.stop()
 
     def set_volume(self, volume):
+        self.volume = volume
         # Set the volume of the music (0.0 to 1.0)
-        pygame.mixer.music.set_volume(volume)
+        self.background_sound.set_volume(self.volume)
         
     def check_music(self):
         return pygame.mixer.music.get_busy()
     
-    def play_sfx(self,sfx_path):
-        pygame.mixer.Channel(1).play(pygame.mixer.Sound(sfx_path))
+    def play_sfx(self):
+        self.sfx.set_volume(self.volume)
+        pygame.mixer.Channel(1).play(self.sfx)
