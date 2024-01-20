3032f76327f5fb1812f575f70c3a1f11885afdbd
markkashraf <mark2001rko2001@gmail.com>
2024-01-20 02:45:53 +0200

    fix

--- states/credit.py
+++ states/credit.py
@@ -20,7 +20,7 @@ class Credit(State):
 
     def render(self, display):
         display.fill(self.game.BLUE)
-        self.helper.draw_text(display, 'Credits', self.game.WHITE, 80, self.game.DISPLAY_W / 2, self.game.global_title_text_size)
+        self.helper.draw_text(display, 'Credits', self.game.WHITE, 80, self.game.DISPLAY_W / 2, self.game.global_title_font_size)
         self.menuGUI.render(display)
 
     def enter_state(self):
